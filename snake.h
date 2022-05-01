f1119e4e52cdbce1efbd1282f07c8878c8f6a977
aaaris <2997600742@qq.com>
2022-05-02 01:33:13 +0800

    create file

---
+++ snake.h
@@ -0,0 +1,27 @@
+#ifndef _SNAKE_H_
+#define _SNAKE_H_
+#include <iostream>
+#include <windows.h>
+
+class bodyNode
+{
+public:
+
+};
+
+class Snake
+{
+public:
+    bodyNode* head;
+    COLOR16 color; 
+};
+
+
+
+
+
+
+
+
+
+#endif
